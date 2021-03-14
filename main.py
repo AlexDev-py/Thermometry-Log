cb231ae4413184303aa3c14626b6e3e8e5e715ca
AlexDev-py <aleks.filiov@yandex.ru>
2021-03-14 14:32:43 +0900

    Изменения в API.

--- main.py
+++ main.py
@@ -2,55 +2,117 @@ import webview
 import web.app
 from sqlite3_api import API
 from database import ThermometryLog, Float
+from datetime import datetime
+import json
 
 
-REAL_URL: str = ...  # Ссылка на локальный сервер с приложением
+class JSApi:
+    """
+    Методы, которые будут доступны для использования из JavaScript`а.
+    """
 
-
-def main():
-    window = webview.create_window(
-        'Журнал термометрии', web.app.app, width=1080, height=720,
-        frameless=True, easy_drag=False
-    )
-    web.app.window = window
-
-    def open_url(url: str):
-        """ Открывает ссылку """
-        print(f'{REAL_URL}/{url}')
-        window.load_url(f'{REAL_URL}/{url}')
-
-    def destroy_app():
-        """ Закрывает приложение """
-        window.destroy()
-
-    def hide_app():
-        """ Сворачивает/разворачивает окно """
-        window.minimize()
-
-    def delete_log(log_id: int):
+    def delete_log(self, log_id: int):
         """ Удаляем запись под номером `log_id` """
-        sql = API(db_path='database.sqlite')
-        sql.execute(f'DELETE FROM thermometrylog WHERE id=?', log_id)
-        sql.commit()
 
-    def edit_log(log_id: int, name: str, temp: float):
+        self.sql.execute(f'DELETE FROM thermometrylog WHERE id=?', log_id)
+        self.sql.commit()
+
+    def edit_log(self, log_id: int, name: str, temp: float):
         """ Изменяем запись под номером `log_id` """
-        ThermometryLog('database.sqlite').filter(id=log_id).update(
+
+        self.thermometry_logs.filter(id=log_id).update(
             name=name,
             temperature=Float(temp)
         )
 
-    window.expose(
-        destroy_app, hide_app, open_url, delete_log, edit_log
-    )
+    def get_logs(self, date: str = None) -> json.dumps:
+        """
+        Получаем записи из базы данных.
+        :param date: Дата в формате `%Y-%m-%d` для фильтрации записей.
+        """
 
-    def _init(win):
-        global REAL_URL
-        REAL_URL = win.real_url
-        print(REAL_URL)
+        if date:
+            date = datetime.strptime(date, '%Y-%m-%d')
+        else:
+            date = datetime.now()
 
-    webview.start(_init, window, debug=True, gui='gt')
+        logs = self.thermometry_logs.filter(
+            date=date.strftime('%d.%m.%Y'),
+            return_list=True,
+            return_type='visual'
+        )
+
+        if len(logs):
+            average_temp = round(
+                sum(map(lambda log: log[2], logs)) / len(logs), 1
+            )
+            min_temp = min(logs, key=lambda x: x[2])[2]
+            max_temp = max(logs, key=lambda x: x[2])[2]
+        else:
+            average_temp = min_temp = max_temp = 0
+
+        return json.dumps(dict(
+            logs=logs,
+            average_temp=average_temp,
+            min_temp=min_temp,
+            max_temp=max_temp
+        ))
+
+    @property
+    def sql(self):
+        """ sqlite3_api.API """
+
+    @sql.getter
+    def sql(self) -> API:
+        """ Получение нового API """
+
+        return API(db_path='database.sqlite')
+
+    @property
+    def thermometry_logs(self):
+        """ database.ThermometryLog """
+
+    @thermometry_logs.getter
+    def thermometry_logs(self) -> ThermometryLog:
+        """ Получение нового ThermometryLog """
+
+        return ThermometryLog(db_path='database.sqlite')
 
 
-if __name__ == '__main__':
-    main()
+def open_url(url: str):
+    """ Открывает ссылку """
+
+    window.load_url(f'{REAL_URL}/{url}')
+
+
+def destroy_app():
+    """ Закрывает приложение """
+
+    window.destroy()
+
+
+def hide_app():
+    """ Сворачивает/разворачивает окно """
+
+    window.minimize()
+
+
+def loaded():
+    """ Вызывается после загрузки DOM. """
+
+    window.evaluate_js('get_logs()')
+
+
+def _init(win):
+    global REAL_URL
+    REAL_URL = win.real_url
+
+
+REAL_URL: str = ...  # Ссылка на локальный сервер с приложением
+web.app.window = window = webview.create_window(
+    'Журнал термометрии', web.app.app, width=1080, height=720,
+    frameless=True, easy_drag=False, js_api=JSApi()
+)
+window.loaded += loaded
+window.expose(open_url, destroy_app, hide_app)
+webview.start(_init, window, debug=True, gui='gt')
--- web/app.py
+++ web/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, jsonify
-from database import ThermometryLog
+from flask import Flask, render_template, request
 from webview import Window
 from datetime import datetime
 
@@ -19,39 +18,4 @@ def home():
         'main.html',
         window=(window.width, window.height),
         date=date.strftime('%Y-%m-%d')
-    )
-
-
-@app.route('/api/getLogs')
-def get_logs():
-    """
-    Получаем записи из базы данных.
-    : param date : Дата в формате `%Y-%m-%d` для фильтрации записей.
-    """
-
-    date = datetime.now()
-    if request.args.get('date'):
-        date = datetime.strptime(request.args.get('date'), '%Y-%m-%d')
-
-    ThermometryLogs = ThermometryLog(db_path='database.sqlite')
-    logs = ThermometryLogs.filter(
-        date=date.strftime('%d.%m.%Y'),
-        return_list=True,
-        return_type='visual'
-    )
-
-    if len(logs):
-        average_temp = round(
-            sum(map(lambda log: log[2], logs)) / len(logs), 1
-        )
-        min_temp = min(logs, key=lambda x: x[2])[2]
-        max_temp = max(logs, key=lambda x: x[2])[2]
-    else:
-        average_temp = min_temp = max_temp = 0
-
-    return jsonify(
-        logs=logs,
-        average_temp=average_temp,
-        min_temp=min_temp,
-        max_temp=max_temp
     )
